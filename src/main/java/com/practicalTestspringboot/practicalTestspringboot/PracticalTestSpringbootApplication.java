50b908954b5f4f98d23a72fcbeff563ff69db8a9
Dilumini De Silva <diluminidesilva@yahoo.com>
2021-09-17 21:19:50 +0530

    Initial commit

---
+++ src/main/java/com/practicalTestspringboot/practicalTestspringboot/PracticalTestSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.practicalTestspringboot.practicalTestspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalTestSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalTestSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practicalTestspringboot/practicalTestspringboot/PracticalTestSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicalTestspringboot.practicalTestspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalTestSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
