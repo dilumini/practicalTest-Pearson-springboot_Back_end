17ce7a0f2a13d65d0955bbe71a6d3ca7925ce41d
Dilumini De Silva <diluminidesilva@yahoo.com>
2021-09-18 14:41:18 +0530

    Create backend with H2 database

--- src/main/java/com/practicalTestspringboot/practicalTestspringboot/service/CountryService.java
+++ src/main/java/com/practicalTestspringboot/practicalTestspringboot/service/CountryService.java
@@ -1,8 +1,20 @@
 package com.practicalTestspringboot.practicalTestspringboot.service;
 
+import com.practicalTestspringboot.practicalTestspringboot.exception.CountryNotFoundException;
 import com.practicalTestspringboot.practicalTestspringboot.model.Country;
+
+import java.util.List;
 
 public interface CountryService {
 
+     Country updateCountryById(Long countryId, Country country);
+
+
     Country addCountry(Country country);
+
+    List<Country> fetchCountryList();
+
+    Country fetchCountryById(Long countryId) throws CountryNotFoundException;
+
+    void deleteCountryById(Long countryId) throws CountryNotFoundException;
 }
--- src/main/java/com/practicalTestspringboot/practicalTestspringboot/service/CountryServiceImpl.java
+++ src/main/java/com/practicalTestspringboot/practicalTestspringboot/service/CountryServiceImpl.java
@@ -1,9 +1,13 @@
 package com.practicalTestspringboot.practicalTestspringboot.service;
 
+import com.practicalTestspringboot.practicalTestspringboot.exception.CountryNotFoundException;
 import com.practicalTestspringboot.practicalTestspringboot.model.Country;
 import com.practicalTestspringboot.practicalTestspringboot.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CountryServiceImpl implements CountryService {
@@ -12,7 +16,37 @@ public class CountryServiceImpl implements CountryService {
     private CountryRepository countryRepository;
 
     @Override
+    public Country updateCountryById(Long countryId, Country country) {
+        return null;
+    }
+
+    @Override
     public Country addCountry(Country country) {
+
         return countryRepository.save(country);
     }
+
+    @Override
+    public List<Country> fetchCountryList() {
+        return countryRepository.findAll();
+    }
+
+    @Override
+    public Country fetchCountryById(Long countryId) throws CountryNotFoundException {
+        Optional<Country> country =
+                countryRepository.findById(countryId);
+
+        if(!country.isPresent()) {
+            throw new CountryNotFoundException("Country Not available");
+        }
+
+        return  country.get();
+    }
+
+    @Override
+    public void deleteCountryById(Long countryId)  {
+        countryRepository.deleteById(countryId);
+    }
+
+
 }
