69eba96bcf98ec031af1509357dc28a8b10704ff
Dilumini De Silva <diluminidesilva@yahoo.com>
2021-09-22 23:00:48 +0530

    create backend with H2 database

--- src/main/java/com/practicalTestspringboot/practicalTestspringboot/controller/CountryController.java
+++ src/main/java/com/practicalTestspringboot/practicalTestspringboot/controller/CountryController.java
@@ -2,13 +2,14 @@ package com.practicalTestspringboot.practicalTestspringboot.controller;
 
 import com.practicalTestspringboot.practicalTestspringboot.exception.CountryNotFoundException;
 import com.practicalTestspringboot.practicalTestspringboot.model.Country;
+import com.practicalTestspringboot.practicalTestspringboot.repository.CountryRepository;
 import com.practicalTestspringboot.practicalTestspringboot.service.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/rest/v2")
 public class CountryController {
@@ -23,7 +24,7 @@ public Country addCountry(@RequestBody Country country){
     return countryService.addCountry(country);
 }
 
-    @GetMapping("/countries")
+    @GetMapping("/country")
     public List<Country> fetchCountryList() {
         return countryService.fetchCountryList();
     }
@@ -44,5 +45,7 @@ public Country updateCountry(@PathVariable("id") Long countryID,
                                    @RequestBody Country country) {
     return countryService.updateCountry(countryID,country);
 }
+
+
 
 }
